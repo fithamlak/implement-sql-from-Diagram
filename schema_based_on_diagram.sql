39d18ca66f7e206a549e910c7c1506a2a5be7e5c
Fithamlak <justiceofgod01@gmail.com>
2023-04-12 00:55:25 +0300

    update schema
    Add  indexes  for all foreign keys based on the reviewer's suggesion

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,6 +15,8 @@ CREATE TABLE medical_histories (
     PRIMARY KEY (id)
 );
 
+CREATE INDEX MEDICAL_INDEX ON medical_histories(patient_id); 
+
 CREATE TABLE treatments (
 	id INT GENERATED ALWAYS AS IDENTITY,
   type	VARCHAR(120), 
@@ -27,8 +29,13 @@ CREATE TABLE medical_histories_treatments(
     treatment_id INTEGER REFERENCES treatments(id),
     PRIMARY KEY (medical_history_id, treatment_id)
 );
+CREATE INDEX MED_HIS_INDEX ON medical_histories_treatments(medical_history_id); 
+CREATE INDEX TREATMENT_INDEX ON medical_histories_treatments(treatment_id); 
 
 CREATE TABLE invoices { id INT GENERATED ALWAYS AS IDENTITY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT REFERENCES medical_histories(id), 
 PRIMARY KEY(id) } 
 
 CREATE TABLE invoice_items { id INT GENERATED ALWAYS AS IDENTITY, unit_price DECIMAL, quantity INT, total_price DECIMAL, invoice_id INT REFERENCES invoices(id), treatment_id INT REFERENCES treatments(id), PRIMARY KEY(id, invoice_id, treatment_id) } 
+
+CREATE INDEX INVOICE_ITEMS_INDEX ON invoice_items(invoice_id); 
+CREATE INDEX TREATMENT_ITEMS_INDEX ON invoice_items(treatment_id); 
