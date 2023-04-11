e84633a4d4f1ea6634ef415f02ed4142ef3765bf
Mirna <ampueromirna8@gmail.com>
2023-04-11 15:36:13 -0500

    add relations

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -27,3 +27,8 @@ CREATE TABLE medical_histories_treatments(
     treatment_id INTEGER REFERENCES treatments(id),
     PRIMARY KEY (medical_history_id, treatment_id)
 );
+
+CREATE TABLE invoices { id INT GENERATED ALWAYS AS IDENTITY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INT REFERENCES medical_histories(id), 
+PRIMARY KEY(id) } 
+
+CREATE TABLE invoice_items { id INT GENERATED ALWAYS AS IDENTITY, unit_price DECIMAL, quantity INT, total_price DECIMAL, invoice_id INT REFERENCES invoices(id), treatment_id INT REFERENCES treatments(id), PRIMARY KEY(id, invoice_id, treatment_id) } 
