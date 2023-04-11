f6091cbaf0de78ef4559fa90aaed6e5f045a2807
Fithamlak <justiceofgod01@gmail.com>
2023-04-11 22:32:29 +0300

    Create medical_histories table basde on the diagram with forien key also

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -5,3 +5,12 @@ CREATE TABLE patients(
   date_of_birth   DATE,
   PRIMARY KEY(id)
 );
+
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(80),
+    PRIMARY KEY (id)
+);
